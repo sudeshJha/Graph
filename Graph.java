3516d8baacfe4e857997468bef45aa531f8326a8
Sudesh Jha <sudeshjha2005@gmail.com>
2026-07-11 20:36:01 +0530

    graph traversing BFS

--- Graph.java
+++ Graph.java
@@ -18,10 +18,33 @@ class Graph{
             return;
       }
 
+      static void BFS(int[][] adj, boolean[] visited){
+            LinkedList<Integer> q = new LinkedList<Integer>();
+
+            q.offer(0);
+
+            while(!q.isEmpty()){
+                  int u = q.poll();
+
+                  if(visited[u]) continue;
+
+                  System.out.println(u);
+                  visited[u] = true;
+
+                  for(int v : adj[u]){
+                        if(visited[v]) continue;
+                        q.offer(v);
+                  }
+            }
+
+            return;
+      }
+
       public static void main(String[] args){
             int[][] adj = {{3},{3},{1},{2,4},{}};
             boolean[] visited = new boolean[adj.length];
 
-            DFS(adj, 0,visited);
+            // DFS(adj, 0,visited);
+            BFS(adj, visited);
       }
 }
