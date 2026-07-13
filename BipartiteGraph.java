a24bcba012a2815a86965b16c8a54f85e6fed532
Sudesh Jha <sudeshjha2005@gmail.com>
2026-07-13 23:53:27 +0530

    Bipartite graph BFS

--- BipartiteGraph.java
+++ BipartiteGraph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 class BipartiteGraph {
       
@@ -16,8 +17,35 @@ class BipartiteGraph {
             
             return true;
       }
+
+      static boolean BFS(ArrayList<ArrayList<Integer>> adj, int[] color, int el, int currentColor){
+        // -1 = not visited 
+        // 0 = white color (visited)
+        // 1 = black color (visited)
+
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        q.offer(el);
+        color[el] = currentColor;
+
+        while(!q.isEmpty()){
+            int u = q.poll();
+
+            for(int v : adj.get(u)){
+                // adjacent node have same color
+                if(color[u] == color[v]) return false;
+
+                // already visited
+                if(color[v] != -1) continue;
+
+                q.offer(v);
+                color[v] = 1 - color[u];
+            }
+        }
+
+        return true;        
+    }
       
-      static boolean isBipartiteDFS(int V, int[][] edges) {
+      static boolean isBipartite(int V, int[][] edges) {
             
             ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
             
@@ -40,8 +68,8 @@ class BipartiteGraph {
             }
             
             for(int u=0;u<V;u++){
-                  if(color[u] == -1 && !DFS(adj,color,u,0)){
-                  return false;
+                  if(color[u] == -1 && !BFS(adj,color,u,0)){
+                        return false;
                   }
             }
             
@@ -53,6 +81,6 @@ class BipartiteGraph {
             int[][] edges = {{0, 3}, {1, 2}, {3, 2}, {0, 2}};
             // int[][] edges = {{0,1},{0,2}};
 
-            System.out.println(isBipartiteDFS(V, edges));
+            System.out.println(isBipartite(V, edges));
       }
 }
