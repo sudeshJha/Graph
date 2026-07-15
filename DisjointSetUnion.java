114589f383340c481b7e6e5e8ae69675c395afc5
Sudesh Jha <sudeshjha2005@gmail.com>
2026-07-15 11:57:38 +0530

    union by rank and find by path compression

--- DisjointSetUnion.java
+++ DisjointSetUnion.java
@@ -8,6 +8,14 @@ class DisjointSetUnion{
             return find(parents[i], parents);
       }
 
+      static int findByPathCompression(int i, int[] parents){
+            if(i == parents[i]){
+                  return i;
+            }
+
+            return parents[i] = findByPathCompression(parents[i], parents);
+      }
+
       static void union(int a, int b, int[] parents){
 
             int parent_a = find(a, parents);
@@ -17,4 +25,31 @@ class DisjointSetUnion{
                   parents[parent_a] = parent_b;
             }
       }
+
+      static void unionByRank(int a, int b, int[] parents, int[] rank){
+            int parent_a = findByPathCompression(a, parents);
+            int parent_b = findByPathCompression(b, parents);
+
+            // both are in same set no need for union
+            if(parent_a == parent_b){
+                  return;
+            }
+            else{
+                  if(rank[parent_a] == rank[parent_b]){
+                        rank[parent_a]++;
+                        parent[parent_b] = parent_a;
+
+                  }
+                  else if(rank[parent_a] > rank[parent_b]){
+                        parents[parent_b] = parent_a;
+                  }
+                  else{
+                        parents[parent_a] = parent_b;
+                  }
+            }
+
+            return;
+      }
+
+      
 }
